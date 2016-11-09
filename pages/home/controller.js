c3d53c05f511dc49c6850568ce6d135a7b86928e
matiasv85 <matiasv85@gmail.com>
2016-11-08 21:11:58 -0300

    Initial commit

--- pages/home/controller.js
+++ pages/home/controller.js
@@ -2,5 +2,17 @@
 var app = angular.module('Jam');
 
 app.controller('HomeController', ['$scope', function($scope){
-
+	$scope.searchText = "Taxiboys";
+	$scope.workers = [
+		{
+			name: "Agustin Labaronnie",
+			profilePhoto: "pages/img/agustin.jpg",
+			description: "Desarrollador de Software, Bajista a domicilio. Juntando plata para comprar una camara con mayor resolucion"
+		},
+		{
+			name: "Juan Mendoza",
+			profilePhoto: "pages/img/juan.jpg",
+			description: "Desarrollador de Software, Technical Lead en Despegar.com, trabajos de plomeria en general. Si te lo cruzas, asegurate de saludarlo"
+		}
+	]
 }]);
